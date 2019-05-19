7e665a75b715aaec937b4fe45a825d68716e2b0c
thaiscpaz <thaispaz58@gmail.com>
2019-05-19 18:35:14 -0300

    criar organizacoes

--- src/api/organizacao/index.js
+++ src/api/organizacao/index.js
@@ -1,8 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const uuid = require('uuid');
+const knex = require('../../mysql').default
+const utils = require('../utils').default
 
-router.get("/", async (req, res) => {
-    res.sendStatus(200);
+router.post("/", async (req, res) => {
+    if(!utils.verify(req.body, ["nome", "cnpj", "email", "logradouro",
+     "numero", "bairro", "cidade", "estado", "telefone", "imagem"])) {
+        return res.sendStatus(400)
+    }
+
+    try {
+
+        const newBody = req.body;
+        const id = uuid.v4(); 
+        const newObj = {
+            idorganizacao: id,
+            data_criacao: new Date(),
+            deletado: false,
+            ...newBody
+        }
+
+        await knex("organizacao").insert(newObj); 
+        const newOrg = await knex("organizacao").select("*").where("idorganizacao", id); 
+
+        return res.json(newOrg);
+    } catch (err) {
+        console.error(err)
+        return res.sendStatus(500);
+    }
+
 });
+
 
 module.exports = router;
